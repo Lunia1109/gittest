242cfeb03d82a9ceee97548ab6a16acbeea7baa5
GDJ <GDJ@DESKTOP-8HNUQMJ>
2024-04-18 11:49:13 +0900

    MJ_Main메소드 작성

---
+++ gittest/src/com/gittest/main/Main.java
@@ -0,0 +1,11 @@
+package com.gittest.main;
+
+// Package Explorer 상 아이콘에 ? 표시가 있다는 것은 현재 폴더(패키지)에 있는 파일(class)이 현재 ignore 파일에 기재도 돼있는 것도 아니고
+// untracked, tracked 등의 설정도 돼있는 것이 아니라는 것을 의미한다.
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
